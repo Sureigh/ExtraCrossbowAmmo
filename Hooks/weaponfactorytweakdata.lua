7c5f3c8133f440c0217da80750f2d2c3d748ffb4
Sleigh <63042359+Sureigh@users.noreply.github.com>
2021-04-26 18:45:27 +0900

    BeardLib is a heck
    i had to change all stat modifications to hooks instead because apparently you can't use floating point integers
    
    thanks beardlib

--- Hooks/weaponfactorytweakdata.lua
+++ Hooks/weaponfactorytweakdata.lua
@@ -12,10 +12,36 @@ Hooks:PostHook( WeaponFactoryTweakData, "init", "ECA_Bolts", function(self)
             standing = {2.4, 3.2, -1.5, 1.5}
         }
     }
+    self.wpn_fps_bow_crossbow_m_rocket.override_weapon_multiply = {
+        AMMO_MAX = 0.65,
+        stats = {
+            damage = 0.8
+        }
+    }
+
+    self.wpn_fps_bow_crossbow_m_rocket.override_weapon_add = {
+        stats = {
+            spread = -4
+        }
+    }
 
     -- Flaming Bolts
     self.wpn_fps_bow_crossbow_m_fire.override_weapon = {
         armor_piercing_chance = 0.5,
         AMMO_PICKUP = {0.8, 1.1}
     }
+
+    self.wpn_fps_bow_crossbow_m_fire.override_weapon_multiply = {
+        AMMO_MAX = 0.8,
+        stats = {
+            damage = 0.95
+        }
+    }
+
+    self.wpn_fps_bow_crossbow_m_fire.override_weapon_add = {
+        stats = {
+            spread = -4
+        }
+    }
+
 end )
